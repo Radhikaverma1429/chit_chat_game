c6429fa787cfc8378366ed95a21e0f836e36a60d
Radhika_0014 <79450354+Radhikaverma1429@users.noreply.github.com>
2022-08-29 19:43:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+// const express = require('express')
+// const app =express()
+//  const http =require('http').createServer(app)
+//  const PORT =process.env.PORT||3000
+
+// var user = prompt("Enter your name,senpai");
+// var nam = document.getElementById("user");
+// nam.innerHTML = user;
+// document.querySelector('#btn').addEventListener("click",meraPyaraFunction);
+// console.log(user);
+// function meraPyaraFunction(){
+//  console.log("por que maria");
+// }
+
+
+var a="radhika";
+console.log(a);
